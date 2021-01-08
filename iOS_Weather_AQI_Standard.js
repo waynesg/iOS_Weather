91b9681bfd383ca5983da11a20990c0291fe8f91
Wayne Zhong <waynesg511@gmail.com>
2021-01-08 23:10:35 +0800

    Update iOS_Weather_AQI_Standard.js

--- iOS_Weather_AQI_Standard.js
+++ iOS_Weather_AQI_Standard.js
@@ -1,9 +1,5 @@
-// Developed by Hackl0us (https://github.com/hackl0us)
-
-// STEP 1: 前往 https://aqicn.org/data-platform/token/ 注册账户，将申请的 API Token 填入下方
 const aqicnToken = 'f8009c61b8bbe2b6f46b795c442fe1eaeffd5fca'
 
-// STEP 2: 参考下方配置片段，在代理工具的配置文件中添加对应的配置。注意：script-path 后应该替换为添加 apicnToken 值后的脚本路径
 /*
 	[Script]
 	AQI-US = type=http-response, pattern=https://weather-data.apple.com/v1/weather/[\w-]+/[0-9]+\.[0-9]+/[0-9]+\.[0-9]+\?include=, requires-body=true, script-path=/path/to/iOS_Weather_AQI_Standard.js
